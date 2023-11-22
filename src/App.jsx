21f968e712db32aa89e22264f178719d274f70a2
DRV111 <xblackcrownx@gmail.com>
2023-11-22 13:24:52 +0300

    feat: add Loader component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import MainLayout from './components/MainLayout';
 import NavBar from './components/NavBar';
 import SearchBar from './components/SearchBar';
@@ -8,13 +8,36 @@ import Box from './components/Box';
 import MovieList from './components/MovieList';
 import Summary from './components/Summary';
 import WatchedMovieList from './components/WatchedMovieList';
-import tempWatchedData from './data/watchedMovies';
-import tempMovieData from './data/moviesList';
+import Loader from './components/Loader';
+// import tempWatchedData from './data/watchedMovies';
+// import tempMovieData from './data/moviesList';
+
+const API_KEY = '106c87f9';
 
 function App() {
-  const [movies, setMovies] = useState(tempMovieData);
-  const [watched, setWatched] = useState(tempWatchedData);
+  const [movies, setMovies] = useState([]);
+  const [watched, setWatched] = useState([]);
   const [query, setQuery] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
+  const movieQuery = 'punisher';
+
+  useEffect(
+    function () {
+      async function fetchmoviesData() {
+        setIsLoading(true);
+        const res = await fetch(
+          `https://www.omdbapi.com/?apikey=${API_KEY}&s=${movieQuery}`
+        );
+        const data = await res.json();
+        setMovies(data.Search);
+        setIsLoading(false);
+      }
+      fetchmoviesData();
+    },
+    [movieQuery]
+  );
+
   return (
     <>
       <NavBar>
@@ -23,9 +46,7 @@ function App() {
         <NumResults movies={movies} />
       </NavBar>
       <MainLayout>
-        <Box>
-          <MovieList movies={movies} />
-        </Box>
+        <Box>{isLoading ? <Loader /> : <MovieList movies={movies} />}</Box>
         <Box>
           <Summary watched={watched} />
           <WatchedMovieList watched={watched} />
---
+++ src/components/Loader.jsx
@@ -0,0 +1,5 @@
+function Loader() {
+  return <p className="loader">LOADING, PLEASE WAIT...</p>;
+}
+
+export default Loader;
