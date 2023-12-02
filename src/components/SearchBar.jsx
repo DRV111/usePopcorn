5ffe6a8e958522b91a33f8461ece0c9cef8f12a7
DRV111 <xblackcrownx@gmail.com>
2023-12-02 14:21:52 +0300

    feat: focus on search bar on app startup

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,4 +1,28 @@
+import { useEffect, useRef } from 'react';
+
 function SearchBar({ query, setQuery }) {
+  const inputEl = useRef(null);
+
+  useEffect(
+    function () {
+      function callBackFn(e) {
+        if (document.activeElement === inputEl.current) {
+          return;
+        }
+
+        if (e.code === 'Enter') {
+          inputEl.current.focus();
+          setQuery('');
+        }
+      }
+      document.addEventListener('keydown', callBackFn);
+      return () => {
+        document.removeEventListener('keydown', callBackFn);
+      };
+    },
+    [setQuery]
+  );
+
   return (
     <input
       className="search"
@@ -6,6 +30,7 @@ function SearchBar({ query, setQuery }) {
       placeholder="Search movies..."
       value={query}
       onChange={(e) => setQuery(e.target.value)}
+      ref={inputEl}
     />
   );
 }
